ae5678fa2cc1114d91301e51323e9f6925176da2
huxiguo <hu@seekhoo.cn>
2023-07-10 11:15:15 +0800

    删除ba

--- src/components/appFooter/index.tsx
+++ src/components/appFooter/index.tsx
@@ -50,16 +50,7 @@ const AppFooter: FC<IProps> = () => {
 							</a>
 						</span>
 					</div>
-					<div className="info">
-						<span>粤B2-20090191-18</span>
-						<a
-							href="http://www.beian.miit.gov.cn/publish/query/indexFirst.action"
-							rel="noopener noreferrer"
-							target="_blank"
-						>
-							工业和信息化部备案管理系统网站
-						</a>
-					</div>
+
 				</FooterLeft>
 				<FooterRight className="right">
 					{footerImages.map(item => {
